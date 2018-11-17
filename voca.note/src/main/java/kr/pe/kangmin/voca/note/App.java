673a86518e31e5c06a1d234e7e2f188289f43f46
kangmin <kangmin@kangmin-gram>
2018-11-18 03:24:53 +0900

    Use selector-syntax to find elements

--- voca.note/src/main/java/kr/pe/kangmin/voca/note/App.java
+++ voca.note/src/main/java/kr/pe/kangmin/voca/note/App.java
@@ -15,9 +15,49 @@ import org.jsoup.select.Elements;
 public class App 
 {
 	public static void main(String [] args) {
-		parseDom();
+		extract();
 	}
 
+	public static void extract() {
+		String html = "<p>An <a href='http://example.com/'><b>example</b></a> link.</p>";
+		Document doc = Jsoup.parse(html);
+		Element link = doc.select("a").first();
+		
+		String text = doc.body().text(); // "An example link"
+		String linkHref = link.attr("href"); // "http://example.com/"
+		String linkText = link.text();  // "example"
+		
+		String linkOuterH = link.outerHtml(); // "<a href="http://example.com"><b>example</b></a>"
+		String linkInnerH = link.html(); // "<b>example</b>
+		
+		System.out.println("TEXT: " + text);
+		System.out.println("HREF LINK : " + linkHref);
+		System.out.println("TEXT LINK: " + linkText);
+		System.out.println("OUTER LINK: " + linkOuterH);
+		System.out.println("INNER LINK: " + linkInnerH);
+	}
+	public static void selector() {
+		// Use selector-syntax to find elements
+		File input = new File("/home/kangmin/play/Project/vocabulary_note/segregate_daum.html");
+		Document doc = null;
+		try {
+			doc = Jsoup.parse(input, "UTF-8");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Elements links = doc.select("a[href]"); // a with href
+		Elements pngs = doc.select("img[src$=.png]"); // img with src ending .png
+		
+		Element masthead = doc.select("div.layer_body").first(); // div with class=masthead
+		
+		Elements resultLinks = doc.select("h3.r > a"); // direct a after h3
+		
+		System.out.println("LINKS <a> tag with href: " + links.size());
+		System.out.println("IMAGE <img> tag with png file: " + pngs.size());
+		System.out.println("RESULT LINKS <h3> tags with <a> tag: " + resultLinks.size());
+		System.out.println("FIRST ELEMENT <div> tag with class \"layer_body\": " + masthead);
+	}
 	public static void parseDom() {
 		// Use DOM methods to navigate a document
 		File input = new File("/home/kangmin/play/Project/vocabulary_note/segregate_daum.html");
